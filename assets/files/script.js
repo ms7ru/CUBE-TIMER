d573c7e322b53f1396f431036aa007b5551e099a
ms7ru <rhythm.upa@gmail.com>
2025-01-04 09:31:36 +0530

    hmm

--- assets/files/script.js
+++ assets/files/script.js
@@ -18,30 +18,26 @@ document.addEventListener("DOMContentLoaded",
         document.getElementById("cube").innerHTML = `
             <select>
             <option value="3x3">3x3</option>
-            <option value="2x2">2x2</option>
-            <option value="4x4">4x4</option>
-            <option value="5x5">5x5</option>
-            <option value="6x6">6x6</option>
-            <option value="7x7">7x7</option>
-            <option value="3BLD">3x3 Blindfolded</option>
-            <option value="3FM">3x3 Fewest Moves</option>
-            <option value="3OH">3x3 One Hand</option>
-            <option value="Clock">Clock</option>
-            <option value="Mega">Megamix</option>
-            <option value="Pyra">Pyraminx</option>
-            <option value="Skewb">Skewb</option>
-            <option value="Square1">Square-1</option>
-            <option value="4BLD">4x4 Blindfolded</option>
-            <option value="5BLD">5x5 Blindfolded</option>
             </select>
         `
-    
-        document.getElementById("mode").innerHTML = `
-            <select>
-            <option value="PC">PC</option>
-            <option value="mobile">Mobile</option>
-            </select>
-            `
+
+        /*
+        <option value="2x2">2x2</option>
+        <option value="4x4">4x4</option>
+        <option value="5x5">5x5</option>
+        <option value="6x6">6x6</option>
+        <option value="7x7">7x7</option>
+        <option value="3BLD">3x3 Blindfolded</option>
+        <option value="3FM">3x3 Fewest Moves</option>
+        <option value="3OH">3x3 One Hand</option>
+        <option value="Clock">Clock</option>
+        <option value="Mega">Megamix</option>
+        <option value="Pyra">Pyraminx</option>
+        <option value="Skewb">Skewb</option>
+        <option value="Square1">Square-1</option>
+        <option value="4BLD">4x4 Blindfolded</option>
+        <option value="5BLD">5x5 Blindfolded</option>
+        */
     
     }
 );
@@ -75,36 +71,21 @@ function startTimer() {
     isRunning = true;
 }
 
+var listsofsolves = {};
+
 function stopTimer() {
+    
     clearInterval(timer);
     isRunning = false;
     display.textContent = "0.000";
     solveno ++;
 
-    statslist.innerHTML = `
-                    <div class="time-item" id="time-item-${solveno}">
-            <table>
-            <tr>
-            <td class="solveno">
-                ${solveno}.
-            </td>
-            <td>
-                <h4 class="timeofsolve" id="solveno-${solveno}">${(elapsedTime / 1000).toFixed(3)}</h4>
-            </td>
-            <td>
-                <a class="plus2" href="javascript:update('2', ${solveno});">+2</a>
-            </td>
-            <td>
-                <a class="dnf" href="javascript:update('DNF', ${solveno});">DNF</a>
-            </td>
-            <td>
-                <a class="delete" href="javascript:update('DELETE', ${solveno});">DELETE</a>
-            </td>
-            </tr>
-            </table>
-                    </div>
-    ${statslist.innerHTML}`
     
+    //listsofsolves.push(elapsedTime / 1000);
+
+    update(elapsedTime / 1000, solveno);
+    
+    elapsedTime = 0;
 }
 
 function updateTimer() {
@@ -120,4 +101,33 @@ function update(time, solveno) {
     } else {
         document.getElementById(`solveno-${solveno}`).textContent = time + 2;
     }
+}
+
+function update(a, b) {
+    statslist.innerHTML = `
+                    <div class="time-item" id="time-item-${b}">
+            <table>
+            <tr>
+            <td class="solveno">
+                ${b}.
+            </td>
+            <td>
+                <h4 class="timeofsolve" id="solveno-${b}">${a}</h4>
+            </td>
+            <td>
+                <a class="plus2" href="javascript:update('2', ${b});">+2</a>
+            </td>
+            <td>
+                <a class="dnf" href="javascript:update('DNF', ${b});">DNF</a>
+            </td>
+            <td>
+                <a class="delete" href="javascript:update('DELETE', ${b});">DELETE</a>
+            </td>
+            </tr>
+            </table>
+                    </div>
+    ${statslist.innerHTML}`
+
+
+    
 }
